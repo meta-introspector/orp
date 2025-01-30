a93352e222e33b5b5a4e7bfb8762ef3027b7c64b
Frédérik Bilhaut <fbp@xineo.net>
2025-01-30 11:19:42 +0100

    initial commit

---
+++ src/model.rs
@@ -0,0 +1,71 @@
+use std::path::Path;
+use composable::Composable;
+use ort::session::{Session, SessionInputs, SessionOutputs, builder::GraphOptimizationLevel};
+use super::Result;
+use super::params::RuntimeParameters;
+use super::pipeline::Pipeline;
+
+
+/// A `Model` can load an ONNX model, and run it using the provided pipeline.
+pub struct Model {    
+    session: Session,
+}
+
+
+impl Model {    
+    pub fn new<P: AsRef<Path>>(model_path: P, params: RuntimeParameters) -> Result<Self> {
+        let session = Session::builder()?
+            .with_intra_threads(params.threads())?
+            .with_execution_providers(params.into_execution_providers())?
+            .with_optimization_level(GraphOptimizationLevel::Level3)?            
+            .commit_from_file(model_path)?;
+
+        Ok(Self {
+            session,
+        })
+    }
+
+    pub fn inference<'a, P: Pipeline<'a>>(&'a self, input: P::Input, pipeline: &P, params: &P::Parameters) -> Result<P::Output> {
+        // pre-process
+        let (input, context) = pipeline.pre_processor(params).apply(input)?;
+        // inference
+        let output = self.run(input)?;                
+        // post-process
+        let output = pipeline.post_processor(params).apply((output, context))?;        
+        // ok
+        Ok(output)
+    }
+
+    pub fn to_composable<'a, P: Pipeline<'a>>(&'a self, pipeline: &'a P, params: &'a P::Parameters) -> impl Composable<P::Input, P::Output> {
+        ComposableModel::new(self, pipeline, params)
+    }
+
+
+    fn run(&self, input: SessionInputs<'_, '_>) -> Result<SessionOutputs<'_, '_>> {
+        Ok(self.session.run(input)?)
+    }
+
+
+}
+
+
+/// References a model, a pipeline and some parameters to implement `Composable`
+struct ComposableModel<'a, P: Pipeline<'a>> {
+    model: &'a Model,
+    pipeline: &'a P,
+    params: &'a P::Parameters,
+}
+
+
+impl<'a, P: Pipeline<'a>> ComposableModel<'a, P> {
+    pub fn new(model: &'a Model, pipeline: &'a P, params: &'a P::Parameters) -> Self {
+        Self { model, pipeline, params }
+    }
+}
+
+
+impl<'a, P: Pipeline<'a>> Composable<P::Input, P::Output> for ComposableModel<'a, P> {
+    fn apply(&self, input: P::Input) -> Result<P::Output> {
+        self.model.inference(input, self.pipeline, self.params)
+    }
+}
